3b1a4c2eceb11b0f48ad3a80d1d8bf3313e406fb
Sac33-ai <santiago.arino@campus.xaloc.org>
2025-09-17 09:15:47 +0200

    ejercicio_1.html

---
+++ ejercicio1.py
@@ -0,0 +1,26 @@
+
+nombre=input("tu nombre")
+apellido2_input("introduce tu segundo apellido")
+apellido2_input("introduce tu segundo apellido")
+apellido2=input("introduce tu segundo apellido")
+nombre_completo=nombre+""+apellido1+""+apellido2
+suma=var2+var3
+var1="xaloc"
+var2=2
+var3=7
+var4=1.5
+suma=var2+var3
+print(f"la suma de {var2} + {var3} es igual a ", suma)
+resta=var2+var3
+print(f"la resta de {var2} - {var3} es igual a ", resta)
+multiplicacion=var2*var3
+print(f"la mutiplicacion de {var2} * {var3} es igual a ", multiplicacion)
+division=var2/var3
+print(f"la division de {var2} / {var3} es igual a ", division)
+Módulo=var2%var3
+print(f"la Módulo de {var2} % {var3} es igual a ", Módulo)
+Exponente=var2
+
+
+
+
